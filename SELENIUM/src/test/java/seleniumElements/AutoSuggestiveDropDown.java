13c5571e4e3e62f2444ac3aca1bec53b4214b838
Nasohin Anzum Hridi <naoshinhridi@gmail.com>
2023-10-03 12:25:50 +0600

    added DynamicDropDown.class and AutoSuggestiveDropDown.java classes

---
+++ SELENIUM/src/test/java/seleniumElements/AutoSuggestiveDropDown.java
@@ -0,0 +1,31 @@
+package seleniumElements;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+import java.util.List;
+
+public class AutoSuggestiveDropDown {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));//implicit wait
+        driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+        driver.findElement(By.id("autosuggest")).sendKeys("ind");
+        Thread.sleep(3000);
+        List<WebElement> options = driver.findElements(By.cssSelector("li[class='ui-menu-item'] a"));
+
+        for (WebElement option: options){
+           if (option.getText().equalsIgnoreCase("India")){
+               option.click();
+               break;
+           }
+        }
+
+    }
+}
